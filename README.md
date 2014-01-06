b5632e60d494b0d0d4e815822b73527c9dacc8f2
Jan Smolders <jan.smolders@isaac.nl>
2014-01-06 10:15:49 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 knockout-dynamic-forms
 ======================
+License: MIT
 
 Knockout Dynamic Forms with Validation
+
+With this code you can generate a form Dynamically based on a JSON file including validation.
+
+JSON file markup (if a field is not required, just leave as an empty string or array):
+
+        { "formElements": [
+            {
+                "Name"              : "Gender",                         // String: Name of field
+                "Element"           : "input",                          // String: Element (Can be: input, textarea, button, select)
+                "Type"              : "radio",                          // String: Type (Can be: text, radio, checkbox, hidden, password, submit)
+                "Options"           : ["Male", "Female", "Unkown"],     // Array: Options (Used by select, radio, checkbox)
+                "Placeholder"       : "",                               // String: A placeholder text
+                "Value"             : "",                               // String/Integer: Default value 
+                "HideField"         : {                                 // Set if a field needs to be hidden if the value of the current element
+                                                                        // meets a certain condition
+                    "HideElement"   : "",                               // String: The element (by name)  that needs to be hidden
+                    "HideCondition" : ""                                // String/REGEX: The condition the value of the current element has to match
+                },
+                "Validation"        : {                                 // Form validation using kockout/validation. All options of KO/validation can be used.
+                    "required"  : "This field is required",             // String/Boolean: Can be true/false or a string with the validation message
+                    "minLength" : 1,                                    // Validation example
+                    "maxLength" : 100,                                  // Validation example
+                    "pattern"   : "^[A-Z0-9].$"                         // Validation REGEX example
+                }
+            }
+        } 
+        
+The HTML generated uses YAML (http://www.yaml.de/) but ofcourse you can alter them.
+Two stubs are included for testing purposes.
+
+Feel free to improve or use. 
+(ps, run on a webserver, locally does not work of course)
---
+++ global/js/knockout-functions.js
@@ -0,0 +1,121 @@
+
+    //Validation configurables
+    //More info: https://github.com/Knockout-Contrib/Knockout-Validation
+    
+    ko.validation.configure({
+        registerExtenders           : true,
+        messagesOnModified          : true,
+        errorsAsTitle               : true,                 // enables/disables showing of errors as title attribute of the target element.
+        errorsAsTitleOnModified     : false,                // shows the error when hovering the input field (decorateElement must be true)
+        messageTemplate             : null,
+        insertMessages              : true,                 // automatically inserts validation messages as <span></span>
+        parseInputAttributes        : false,                // parses the HTML5 validation attribute from a form element and adds that to the object
+        writeInputAttributes        : false,                // adds HTML5 input validation attributes to form elements that ko observable's are bound to
+        decorateInputElement        : false,                // false to keep backward compatibility
+        decorateElementOnModified   : true,                 // true to keep backward compatibility
+        errorClass                  : null,                 // single class for error message and element
+        errorElementClass           : 'validationElement',  // class to decorate error element
+        errorMessageClass           : 'validationMessage',  // class to decorate error message
+        allowHtmlMessages           : false,                // allows HTML in validation messages
+        grouping: {
+            deep        : false,                            //by default grouping is shallow
+            observable  : true,                             //and using observables
+            live        : false                             //react to changes to observableArrays if observable === true
+        }
+    });
+
+    ko.dirtyFlag = function(root, isInitiallyDirty) {
+        var result = function() {},
+            _initialState       =   ko.observable(ko.toJSON(root)),
+            _isInitiallyDirty   =   ko.observable(isInitiallyDirty);
+
+        result.isDirty = ko.computed(function() {
+            return _isInitiallyDirty() || _initialState() !== ko.toJSON(root);
+        });
+
+        result.reset = function() {
+            _initialState(ko.toJSON(root));
+            _isInitiallyDirty(false);
+        };
+
+        return result;
+    };
+
+    addEventListener('load', function () {
+        doAjaxCall(JSONUrl, function(data){
+            var ajaxResult = JSON.parse(data);
+            ko.applyBindings(new ViewModel(ajaxResult.formElements));
+        });
+    });
+
+
+    function FormField(data) {
+        var self            =   this;
+        self.Name           =   ko.observable(data.Name).extend({required:true});;
+        self.Element        =   ko.observable(data.Element);
+        self.Type           =   ko.observable(data.Type);
+        self.Options        =   ko.observableArray(data.Options);
+        self.Placeholder    =   ko.observable(data.Placeholder);
+        self.Value          =   ko.observable(data.Value).extend(data.Validation);
+        self.HideElement    =   ko.observable(data.HideField.HideElement);
+        self.HideCondition  =   ko.observable(data.HideField.HideCondition);
+    }
+
+    function ViewModel(data) {
+        var self = this;
+
+        self.Fields = ko.observableArray(ko.utils.arrayMap(data, function(item) {
+            return new FormField(item);
+        }));
+
+        self.errors = ko.validation.group(this, { 
+            deep        : true, 
+            observable  : false 
+        });
+        
+        self.SaveForm = function () {
+            if(self.errors().length > 0) {
+                self.errors.showAllMessages();
+                return;
+            }
+        };
+
+        self.HiddenElements = ko.observableArray([]);
+        self.dirtyFlag = new ko.dirtyFlag(this);
+        self.isDirty = ko.computed(function (){
+            if (self.dirtyFlag.isDirty()){
+            
+                var formElements = self.Fields();
+
+                for (var i=0;i<formElements.length;i++){
+
+                    var ElementValue            =   formElements[i].Value();
+                    var HideElement             =   formElements[i].HideElement();
+                    var HideCondition           =   formElements[i].HideCondition();
+
+                    if( HideElement !== ""){
+                        for (var i=0;i<formElements.length;i++){
+                            if(formElements[i].Name() === HideElement ){
+                                if( ElementValue.match(HideCondition) ){
+                                    self.HiddenElements([HideElement]);
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        });
+    }
+
+    function doAjaxCall(url, callback){
+         var xmlhttp;
+         xmlhttp = new XMLHttpRequest();
+         xmlhttp.onreadystatechange = function(){
+             if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+                callback(xmlhttp.responseText);
+             }
+         }
+         xmlhttp.open("GET", url, true);
+         xmlhttp.send();
+    }
